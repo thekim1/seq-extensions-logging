db2077aad8cc7a13bf1f9979affb3088389f20a0
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2025-10-09 09:27:26 +1000

    No need to duplicate solution build

--- Build.ps1
+++ Build.ps1
@@ -42,10 +42,6 @@ foreach ($src in Get-ChildItem src/*) {
     Pop-Location
 }
 
-Write-Output "build: Checking complete solution builds"
-& dotnet build
-if($LASTEXITCODE -ne 0) { throw "Solution build failed" }
-
 foreach ($test in Get-ChildItem test/*.Tests) {
     Push-Location $test
 
